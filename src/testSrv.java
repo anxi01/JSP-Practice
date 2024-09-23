7bbd4e413a43d62020353b403581a889cba2151c
anxi01 <tjdals2893@naver.com>
2024-09-23 14:43:08 +0900

    [3주차]: 한글 사용시 인코딩을 utf-8로 설정한다

--- src/testSrv.java
+++ src/testSrv.java
@@ -11,13 +11,15 @@ public class testSrv extends HttpServlet {
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    // 응답의 컨텐트 타입을 명시적으로 지정한다.
-    resp.setContentType("text/html");
+
+    // 한글을 사용할 경우 UTF-8로 지정한다. (출력 형태 변경)
+    resp.setCharacterEncoding("UTF-8");
+    resp.setContentType("text/html; charset=utf-8");
 
     PrintWriter out = resp.getWriter();
     out.println("<html><head></head><body>");
     for (int i = 0; i < 5; i++) {
-      out.println("Hello" + i + "<br>");
+      out.println("Hello" + i + "안녕" + "<br>");
     }
     out.println("</body></html>");
   }
