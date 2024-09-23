790f4631a42ba1f0f6134f3f3810cb5c297062bf
anxi01 <tjdals2893@naver.com>
2024-09-23 14:40:59 +0900

    [3주차]: 웹 브라우저에 html 생성한다

--- src/testSrv.java
+++ src/testSrv.java
@@ -11,7 +11,14 @@ public class testSrv extends HttpServlet {
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    // 응답의 컨텐트 타입을 명시적으로 지정한다.
+    resp.setContentType("text/html");
+
     PrintWriter out = resp.getWriter();
-    out.println("Hello");
+    out.println("<html><head></head><body>");
+    for (int i = 0; i < 5; i++) {
+      out.println("Hello" + i + "<br>");
+    }
+    out.println("</body></html>");
   }
 }
