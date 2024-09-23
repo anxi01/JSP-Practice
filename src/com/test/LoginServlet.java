357ecf6c20340a1d8f31324c69e08c41aa0d3cca
anxi01 <tjdals2893@naver.com>
2024-09-23 18:38:20 +0900

    [3주차]: 로그인 서블릿 틀을 생성한다

---
+++ src/com/test/LoginServlet.java
@@ -0,0 +1,32 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+  public LoginServlet() {
+    super();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setCharacterEncoding("UTF-8");
+    resp.setContentType("text/html; charset=utf-8");
+    PrintWriter out = resp.getWriter();
+    out.println("<html><head></head><body>");
+    out.println("Login Serv " + "<br>");
+    out.println("</body></html>");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    doGet(req, resp);
+  }
+}
